4c5a6d0e7703efe9532f3eacdef85779dc9169b3
ohuton55 <tttt.tttt.no@gmail.com>
2022-04-23 11:26:28 +0900

    Made a timer and a clock!

---
+++ 30Count/30secCount.js
@@ -0,0 +1,75 @@
+function getImage() {
+    return
+    require("heatshrink").decompress(atob("mUywkEogA6IggWRoBeGLZ4XBgMjn//mcQDJ4XBiX/AAfzDJwXBiYXEAAPyJpsAgQXGAAMgQJdAgJeBAA/xGRcAj4XIMoK0KGAIsHNIZ+CGQ4wHCIQFCmAyJgBhFLoYiCDBNASQ3wCAUPAwpLFoBJGDCBJGJRhLESYyoEEYQYIMQ6uLJYlAK4QAKcAgYFexRpGDAsAl4YMkAYFMgSUHAAvyC4wYP+Z6FDAoXKmYXIDBfyiIVIDBiOGDCC/GDBZ8FUw4YPGB4YDcAnwDCNAj4YDU5IAFggYCh4YDGB4YDgTDDBgcD/8xDBdEgLEHHBYXCSwgYDGAIABmAYLPoaUDNYacHJIZkEDChLDJR4XEDAIrBDAaEDMRZLCCQKuGGA5JFGQR+BbxoXGGQJlBiD2OGQ8CYBAwLGQcBGCgZDC6oZLC5oZJC56BDO5oAx"));
+}
+
+function getBgimg() {
+    g.drawImage(require("Storage").read("myapps_bg.img"), 0, 0);
+    g.setColor(1, 1, 1);
+    g.setFont("Vector", 40);
+    //g.clearRect(5,40,60,80);
+}
+
+var ang = 0;
+function draw() {
+    ang += 0.1;
+    g.drawImage(getImage(), 160, 160, { scale: 0.7, rotate: ang });
+}
+//Testing to draw rotate icon
+setInterval(draw, 170);
+
+
+//First Application(Timer)
+var counter = 30;
+var counterInterval;
+
+function outOfTime() {
+    //E.showMessage("Out of Time!", "My Timer");
+    Bangle.buzz();
+    Bangle.beep(200, 3000);
+    //.then(() => new Promice(resolve => setTimeout(resolve,200)))
+    //.then(() => Bangle.beep(200, 3000));
+    // again, 10 secs later
+    //setTimeout(outOfTime, 10000);
+    getBgimg();
+    //g.setFont("6x8",3);
+    //g.drawString("button",90,60);
+    //g.drawString("= again",90,90);
+}
+
+function countDown() {
+    counter--;
+    // Out of time
+    if (counter <= 0) {
+        clearInterval(counterInterval);
+        counterInterval = undefined;
+        outOfTime();
+        getBgimg();
+        g.drawString("Out", 130, 60);
+        g.drawString("of", 130, 100);
+        g.drawString("Time!", 95, 140);
+        setWatch(startTimer, BTN1);
+        return;
+    }
+    g.clearRect(5, 40, 60, 80);
+    g.setFontAlign(0, 0);
+    g.setFont("6x8", 4);
+    g.setColor(1, 1, 1);
+    g.drawString(counter, 35, 60);
+    //Bangle.setLCDPower(1);
+    g.setColor(0, 0, 0);
+    g.drawString("Now", 130, 60);
+    g.setFont("6x8", 3);
+    g.drawString("Counting", 90, 100);
+}
+
+
+function startTimer() {
+    counter = 30;
+    countDown();
+    getBgimg();
+    if (!counterInterval)
+        counterInterval = setInterval(countDown, 1000);
+}
+
+startTimer();
