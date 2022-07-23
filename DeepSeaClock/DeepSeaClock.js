38fd6125aebef5970943a5b70c98ac7c8a900478
kuma <40682653+ohuton55@users.noreply.github.com>
2022-07-23 10:19:42 +0900

    Added functionality!
    Notify you to rest once an hour.

--- DeepSeaClock/DeepSeaClock.js
+++ DeepSeaClock/DeepSeaClock.js
@@ -2,86 +2,88 @@ var bgimg = {
     width: 170, height: 32, bpp: 1,
     transparent: 0,
     buffer: require("heatshrink").decompress((atob("AC0ICicDFSnECaUEmOACiMBxlgFSVqjAUSjUGCiUG2IUSisshQURgpABP5cVPwta4GgChUCEIcEgFWmOoHJYhGkBOMwQ+EgNjjR4Mc4hoB6wTKkUFoBZDgEQIBwAEkuttQNKgNVAocYhtrrXQCpUqSgcgodC6G2CZMQhSUDgMb/+3/uAhu27dsSY2oFQUDpWv3t9sEFqtK1QMB7dt23APIOwmwHB2vdpfv+3b92hqtQgFVqmq1MQqtMqtVlW+2da6/3pf6wM2/dt3Wqip8BitT3379vd7kW2+r0N9qAPB1W/gJaB9uFrnv9NVuHJutt/cBgmq336wtX4A+BrtQg31+kq1Xc5trvX4g0FwtAjkA9X/vuq1UB1Vy1Xfu2wrX+/237GB+sqgF2AQMh9v2/94SgPxoAGB3zEBjc21fbu2NitS1W7q/V+3fq/rmuv9dhMwOdqGggVh29279VrXr1Fs7/85/vvf7m0Nq+oztV9HoqsKwcd1talW3uipB73+wtZ0GHtnYsl9lWr8tFs8/+/L94="))
-    )
-};
+)};
 var img11 = {
-    width: 16, height: 16, bpp: 1,
-    transparent: 0,
-    buffer: require("heatshrink").decompress((atob("gGAgGggGpgH5h/+hP/hP+h///+qv+tlWtj/4gOAgNAgOggNQ"))
-    )
-};
+  width : 16, height : 16, bpp : 1,
+  transparent : 0,
+  buffer : require("heatshrink").decompress((atob("gGAgGggGpgH5h/+hP/hP+h///+qv+tlWtj/4gOAgNAgOggNQ"))
+)};
 var img12 = {
-    width: 16, height: 16, bpp: 1,
-    transparent: 0,
-    buffer: require("heatshrink").decompress((atob("AAVAgGVgH5h/+hP/hP+h+r/+uvWtlWtj/4gGggFQgEoDgIA="))
-    )
-};
+  width : 16, height : 16, bpp : 1,
+  transparent : 0,
+  buffer : require("heatshrink").decompress((atob("AAVAgGVgH5h/+hP/hP+h+r/+uvWtlWtj/4gGggFQgEoDgIA="))
+)};
 var img21 = {
-    width: 32, height: 32, bpp: 1,
-    transparent: 0,
-    buffer: require("heatshrink").decompress((atob("ACkDAY0EAYUIAYUSAQN8kgDB2lJgFA12SgGhhf8gMCgf//2H1f///8AQP9q8BqGRqQDBpEBAYMggEBoEQAYQ0BgI2CAYYAfA"))
-    )
-};
+  width : 32, height : 32, bpp : 1,
+  transparent : 0,
+  buffer : require("heatshrink").decompress((atob("ACkDAY0EAYUIAYUSAQN8kgDB2lJgFA12SgGhhf8gMCgf//2H1f///8AQP9q8BqGRqQDBpEBAYMggEBoEQAYQ0BgI2CAYYAfA"))
+)};
 var img22 = {
-    width: 32, height: 32, bpp: 1,
-    transparent: 0,
-    buffer: require("heatshrink").decompress((atob("ACkBAYUCAYUEAYUIAYUSAYUkAQN8pMAoO2yUA0Wv/kBocL//+wmr/4AD/tUgWgyNTAYNIgQDBkEBgWAiA6BwA0BHwYDDAD4="))
-    )
-};
+  width : 32, height : 32, bpp : 1,
+  transparent : 0,
+  buffer : require("heatshrink").decompress((atob("ACkBAYUCAYUEAYUIAYUSAYUkAQN8pMAoO2yUA0Wv/kBocL//+wmr/4AD/tUgWgyNTAYNIgQDBkEBgWAiA6BwA0BHwYDDAD4="))
+)};
 var img31 = {
-    width: 32, height: 32, bpp: 1,
-    transparent: 0,
-    buffer: require("heatshrink").decompress((atob("ACcB//+gECAoMAhgKB4EQj/gkEQsEYAYOM4kgiMs6UgiUs6IDB1nVAYOoqoDE+AjBnkFkmSwF5AYN4gIDBgE+kmRCQMkyEAj/Ex/AgEEwBFB/F+NbgA="))
-    )
-};
+  width : 32, height : 32, bpp : 1,
+  transparent : 0,
+  buffer : require("heatshrink").decompress((atob("ACcB//+gECAoMAhgKB4EQj/gkEQsEYAYOM4kgiMs6UgiUs6IDB1nVAYOoqoDE+AjBnkFkmSwF5AYN4gIDBgE+kmRCQMkyEAj/Ex/AgEEwBFB/F+NbgA="))
+)};
 var img32 = {
-    width: 32, height: 32, bpp: 1,
-    transparent: 0,
-    buffer: require("heatshrink").decompress((atob("ACE//gDBoECAYOAgMAgMP+GAgUQhANBkVEAYNKqQDB1VVAaEIFIMQiWkktIAZ8CAYIfBAYeSpIDEgADBFIMSpBuQ"))
-    )
-};
+  width : 32, height : 32, bpp : 1,
+  transparent : 0,
+  buffer : require("heatshrink").decompress((atob("ACE//gDBoECAYOAgMAgMP+GAgUQhANBkVEAYNKqQDB1VVAaEIFIMQiWkktIAZ8CAYIfBAYeSpIDEgADBFIMSpBuQ"))
+)};
 var img41 = {
-    width: 32, height: 32, bpp: 1,
-    transparent: 0,
-    buffer: require("heatshrink").decompress((atob("AE/AAYX/8EH8EAi0KyF/+v/pEAh//8kf////wHC/1/A4PkgEVCYQbCEYYrDAE4A="))
-    )
-};
+  width : 32, height : 32, bpp : 1,
+  transparent : 0,
+  buffer : require("heatshrink").decompress((atob("AE/AAYX/8EH8EAi0KyF/+v/pEAh//8kf////wHC/1/A4PkgEVCYQbCEYYrDAE4A="))
+)};
 var img42 = {
-    width: 32, height: 32, bpp: 1,
-    transparent: 0,
-    buffer: require("heatshrink").decompress((atob("AEfgAQOA//ggNBgEQgWhn/2glQgEV/8Ih////kgAEB/wHC/wHCBYITCDYgjCFYYzDLcY"))
-    )
-};
+  width : 32, height : 32, bpp : 1,
+  transparent : 0,
+  buffer : require("heatshrink").decompress((atob("AEfgAQOA//ggNBgEQgWhn/2glQgEV/8Ih////kgAEB/wHC/wHCBYITCDYgjCFYYzDLcY"))
+)};
 var img51 = {
-    width: 8, height: 8, bpp: 1,
-    transparent: 0,
-    buffer: require("heatshrink").decompress((atob("oHcn96jkZn8w"))
-    )
-};
+  width : 8, height : 8, bpp : 1,
+  transparent : 0,
+  buffer : require("heatshrink").decompress((atob("oHcn96jkZn8w"))
+)};
 var img52 = {
-    width: 8, height: 8, bpp: 1,
-    transparent: 0,
-    buffer: require("heatshrink").decompress((atob("wFc386rkZj8Y"))
-    )
-};
+  width : 8, height : 8, bpp : 1,
+  transparent : 0,
+  buffer : require("heatshrink").decompress((atob("wFc386rkZj8Y"))
+)};
 var img61 = {
-    width: 32, height: 32, bpp: 1,
-    transparent: 0,
-    buffer: require("heatshrink").decompress((atob("AFkKyVA///kmQ34EBktXAYP+gdVqv91YQC/8A1WQv0AqEAoEAlEAn2AigoDAYUEAYUCKUAA="))
-    )
-};
+  width : 32, height : 32, bpp : 1,
+  transparent : 0,
+  buffer : require("heatshrink").decompress((atob("AFkKyVA///kmQ34EBktXAYP+gdVqv91YQC/8A1WQv0AqEAoEAlEAn2AigoDAYUEAYUCKUAA="))
+)};
 var img62 = {
-    width: 32, height: 32, bpp: 1,
-    transparent: 0,
-    buffer: require("heatshrink").decompress((atob("AFkVkmA///kml34EBn1XAYP9gdVqvkgYHBqGrgGlpH/gFQgF+gEogFAgEUgEggEIgEhgEEgEawAyBgJSeA=="))
-    )
-};
+  width : 32, height : 32, bpp : 1,
+  transparent : 0,
+  buffer : require("heatshrink").decompress((atob("AFkVkmA///kml34EBn1XAYP9gdVqvkgYHBqGrgGlpH/gFQgF+gEogFAgEUgEggEIgEhgEEgEawAyBgJSeA=="))
+)};
+var moku = {
+  width : 32, height : 38, bpp : 1,
+  transparent : 0,
+  buffer : require("heatshrink").decompress((atob("AAvAAQMBAYMDgfA8EHgeA8EPg+BAYfAh0HwfAjwDOj4DFD4QDCw/BAYOD4PwBQIDD8ADS4ADDKYQnBAYJPCAYOAMgJuCgSRh"))
+)};
+var coffee = {
+  width : 64, height : 64, bpp : 1,
+  transparent : 0,
+  buffer : require("heatshrink").decompress((atob("AGN////AwcDAwP//AHCj4HC/wWFAAOAA4IGD//ggEB//wEQQDDEYYHBg4rDAgUHBQI7D8AHG/AXF//8g5DDJYP+BQPAIYf+LwYGC/4HF/E/A5vwA59/N4IHEvwHG/wHFK4IHHS4qxCA4p9BV4hsCA4q/GPYgeEN40Ah4+Gcwf//onBn+HCwd/A4IgD+EfwBwE+EP4F8HAbVB4AxBJAXAgfgA4nwA4JJBF4YPDA44XFF4JRCF4V+NIT6BH4M/wD/CKoOAj6CCRgQRBFAIABgYZBg//DAU/P4JmBAYILBJoKnISAgjDUwgACVIQGDAFwA="))
+)};
 const fish1 = [img11, img12];
 const fish2 = [img21, img22];
 const fish3 = [img31, img32];
 const fish4 = [img41, img42];
 const fish5 = [img51, img52];
 const fish6 = [img61, img62];
+var coffeeMsg = "let's take a break!";
+var mokuY = 80;
+var cMsgX = 100;
+var msgCnt1 = 0;
+var msgCnt2 = 0;
 x1 = 155;
 y1 = 145;
 x2 = 180;
@@ -102,123 +104,156 @@ let i5 = 1;
 let i6 = 1;
 require("FontCopasetic40x58Numeric").add(Graphics);
 function Fishers() {
-    g.clearRect(0, 22, 176, 176);
-    var date = new Date();
-    var month = date.getMonth() + 1;
-    var day = date.getDate();
-    var dayOfWeek = date.getDay();
-    var dayOfWeekStr = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"][dayOfWeek];
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    g.clearRect(x1, y1, x1 + 30, y1 + 30);
-    g.clearRect(x2, y2, x2 + 46, y2 + 32);
-    g.clearRect(x3, y3, x3 + 30, y3 + 35);
-    g.clearRect(x4 - 20, y4 + 5, x4 + 40, y4 + 30);
-    g.clearRect(x5 - 20, y5 - 20, x5 + 20, y5 + 20);
-    g.clearRect(x6, y6, x6 + 32, y6 + 32);
-    if (i1 == 2) {
-        i1 = 0;
+  g.clearRect(0,22,176,176);
+  var date = new Date();
+  var month = date.getMonth() + 1;
+  var day = date.getDate();
+  var dayOfWeek = date.getDay();
+  var dayOfWeekStr = ["sun","mon","tue","wed","thu","fri","sat"][dayOfWeek];
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  g.clearRect(x1,y1,x1+30,y1+30);
+  g.clearRect(x2,y2,x2+46,y2+32);
+  g.clearRect(x3,y3,x3+30,y3+35);
+  g.clearRect(x4-20,y4+5,x4+40,y4+30);
+  g.clearRect(x5-20,y5-20,x5+20,y5+20);
+  g.clearRect(x6,y6,x6+32,y6+32);
+  if (i1 == 2) {
+    i1 = 0;
+  }
+  if (i2 == 2) {
+    i2 = 0;
+  }
+  if (i3 == 2) {
+    i3 = 0;
+  }
+  if (i4 == 2) {
+    i4 = 0;
+  }
+  if (i5 == 2) {
+    i5 = 0;
+  }
+  if (i6 == 2) {
+    i6 = 0;
+  }
+  g.setColor(1,1,1);
+  g.drawImage(fish1[i1++],x1,y1,{scale:1.2});
+  g.drawImage(fish2[i2++],x2,y2,{scale:1.3});
+  g.drawImage(fish3[i3++],x3,y3,{scale:0.8});
+  g.drawImage(fish4[i4++],x4,y4,{scale:0.9});
+  g.drawImage(fish5[i5++],x5,y5,{scale:1.3});
+  g.drawImage(fish6[i6++],x6,y6);
+  g.drawImage(bgimg,0,145);
+  x1 = x1 - 3;
+  x2 = x2 - 3;
+  y2 = y2 - 1;
+  y3 = y3 - 2;
+  x4 = x4 + 5;
+  x5 = x5 + 1;
+  y5 = y5 + 5;
+  x6 = x6 - 2;
+  y6 = y6 - 4;
+  if (x1 <= -60){
+    x1 = 185;
+    y1 = 145;
+  }
+  if (x1 <= 100 && x1 >= 0) {
+    x1 = x1 - 5;
+    y1 = y1 - 5;
+  }
+  if (x2 <= -60){
+    x2 = 280;
+    y2 = 80;
+  }
+  if (y3 >= -20 && y3 <= 120) {
+    y3 = y3 - 10;
+  }
+  if (y3 <= -60){
+    x3 = 0;
+    y3 = 200;
+  }
+  if (x4 >= 60 && x4 <= 160) {
+    x4 = x4 + 15;
+  }
+  if (x4 >= 190) {
+    x4 = -100;
+    y4 = 20;
+  }
+  if (x5 => 0 && x5 <= 10) {
+    x5 = x5 + 10;
+    y5 = y5 + 10;
+  }
+  if (x5 > 100 && x5 <= 140) {
+    x5 = x5 + 10;
+    y5 = y5 - 10;
+  }
+  if (x5 > 200) {
+    x5 = -30;
+    y5 = -70;
+  }
+  if (0 <= y6 && y6 <= 100) {
+    x6 = x6 - 5;
+    y6 = y6 + 20;
+    if (y6 >= 90) {
+      x6 = x6 - 15;
+      y6 = y6 - 15;
     }
-    if (i2 == 2) {
-        i2 = 0;
+  }
+  if (x6 <= -150) {
+    x6 = 200;
+    y6 = 200;
+  }
+  g.setFont("Copasetic40x58Numeric");
+  g.drawString(hours,g.getWidth()/4.5,g.getHeight()/3.5, true);
+  g.drawString(minutes,g.getWidth()/2,g.getHeight()/2.5, true);
+  g.setFont("6x8",2);
+  g.drawString(month + "/" + day + " " + dayOfWeekStr,50,135, true);
+  if (minutes >= 0 && minutes <= 5){
+
+    g.clearRect(14,25,160,160);
+    g.drawImage(moku,73,mokuY,{scale:0.8});
+    g.clearRect(65,90,100,100);
+    g.drawImage(coffee,55,80,true);
+    g.drawString(coffeeMsg,cMsgX,140,true);
+    g.clearRect(0,140,14,155);
+    g.clearRect(160,140,180,155);
+    g.drawString(hours + ":" + minutes,17,27,true);
+    g.drawRect(14,25,160,160);
+
+    if (msgCnt1 <= 1){
+      Bangle.buzz();
+      msgCnt1++;
     }
-    if (i3 == 2) {
-        i3 = 0;
+    if (minutes == 5){
+      if (msgCnt2 <= 1){
+        Bangle.buzz();
+        msgCnt2++;
+      }
     }
-    if (i4 == 2) {
-        i4 = 0;
+
+    mokuY = mokuY - 2;
+    cMsgX = cMsgX - 7;
+
+    if (mokuY <= 50){
+      mokuY = 80;
     }
-    if (i5 == 2) {
-        i5 = 0;
+    if (cMsgX <= -100){
+      cMsgX = 180;
     }
-    if (i6 == 2) {
-        i6 = 0;
-    }
-    g.setColor(1, 1, 1);
-    g.drawImage(fish1[i1++], x1, y1, { scale: 1.2 });
-    g.drawImage(fish2[i2++], x2, y2, { scale: 1.3 });
-    g.drawImage(fish3[i3++], x3, y3, { scale: 0.8 });
-    g.drawImage(fish4[i4++], x4, y4, { scale: 0.9 });
-    g.drawImage(fish5[i5++], x5, y5, { scale: 1.3 });
-    g.drawImage(fish6[i6++], x6, y6);
-    g.drawImage(bgimg, 0, 145);
-    x1 = x1 - 3;
-    x2 = x2 - 3;
-    y2 = y2 - 1;
-    y3 = y3 - 2;
-    x4 = x4 + 5;
-    x5 = x5 + 1;
-    y5 = y5 + 5;
-    x6 = x6 - 2;
-    y6 = y6 - 4;
-    if (x1 <= -60) {
-        x1 = 185;
-        y1 = 145;
-    }
-    if (x1 <= 100 && x1 >= 0) {
-        x1 = x1 - 5;
-        y1 = y1 - 5;
-    }
-    if (x2 <= -60) {
-        x2 = 280;
-        y2 = 80;
-    }
-    if (y3 >= -20 && y3 <= 120) {
-        y3 = y3 - 10;
-    }
-    if (y3 <= -60) {
-        x3 = 0;
-        y3 = 200;
-    }
-    if (x4 >= 60 && x4 <= 160) {
-        x4 = x4 + 15;
-    }
-    if (x4 >= 190) {
-        x4 = -100;
-        y4 = 20;
-    }
-    if (x5 => 0 && x5 <= 10) {
-        x5 = x5 + 10;
-        y5 = y5 + 10;
-    }
-    if (x5 > 100 && x5 <= 140) {
-        x5 = x5 + 10;
-        y5 = y5 - 10;
-    }
-    if (x5 > 200) {
-        x5 = -30;
-        y5 = -70;
-    }
-    if (0 <= y6 && y6 <= 100) {
-        x6 = x6 - 5;
-        y6 = y6 + 20;
-        if (y6 >= 90) {
-            x6 = x6 - 15;
-            y6 = y6 - 15;
-        }
-    }
-    if (x6 <= -150) {
-        x6 = 200;
-        y6 = 200;
-    }
-    g.setFont("Copasetic40x58Numeric");
-    g.drawString(hours, g.getWidth() / 4.5, g.getHeight() / 3.5, true);
-    g.drawString(minutes, g.getWidth() / 2, g.getHeight() / 2.5, true);
-    g.setFont("6x8", 2);
-    g.drawString(month + "/" + day + " " + dayOfWeekStr, 50, 135, true);
+  }
 }
 g.clear();
 Fishers();
 var secondInterval = setInterval(Fishers, 500);
 
-Bangle.on('lcdPower', on => {
-    if (secondInterval) clearInterval(secondInterval);
+Bangle.on('lcdPower',on=>{
+  if (secondInterval) clearInterval(secondInterval);
     secondInterval = undefined;
-    if (on) {
-        secondInterval = setInterval(Fishers, 500);
-        g.clear();
-        Fishers();
-    }
+  if (on) {
+    secondInterval = setInterval(Fishers, 500);
+    g.clear();
+    Fishers();
+  }
 });
 Bangle.setUI("clock");
 Bangle.loadWidgets();
